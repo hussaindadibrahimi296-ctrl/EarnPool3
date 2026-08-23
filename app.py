30f3ba5c4f54344047b8feb9597fb4c15c15d04e
hussaindadibrahimi296-ctrl <hussaindadibrahimi296@gmail.com>
2026-08-23 10:44:28 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "EarnPool is running!"
+    return render_template("index.html")
 
 
 @app.route("/api/status")
