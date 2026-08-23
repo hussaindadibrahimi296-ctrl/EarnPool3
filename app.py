8704ebbc65007886ae43a2311505468cc2726a3c
hussaindadibrahimi296-ctrl <hussaindadibrahimi296@gmail.com>
2026-08-23 10:36:30 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "EarnPool is running!"
+
+
+@app.route("/api/status")
+def status():
+    return jsonify({
+        "success": True,
+        "message": "EarnPool API is working"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
